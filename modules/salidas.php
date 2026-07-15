b8ed035d1c69e8cddae9fba04e46112db9724ded
Baxter <gustavomgonzalez16@gmail.com>
2026-07-14 21:55:09 -0400

    fix: cedula V sin guion, solo puntos

--- modules/salidas.php
+++ modules/salidas.php
@@ -550,8 +550,7 @@ unset($_SESSION['flash_msg']);
             if (tipo === 'V') {
                 if (nums.length <= 2) return nums;
                 if (nums.length <= 5) return nums.slice(0,2) + '.' + nums.slice(2);
-                if (nums.length <= 8) return nums.slice(0,2) + '.' + nums.slice(2,5) + '.' + nums.slice(5);
-                return nums.slice(0,2) + '.' + nums.slice(2,5) + '.' + nums.slice(5,8) + '-' + nums.slice(8,9);
+                return nums.slice(0,2) + '.' + nums.slice(2,5) + '.' + nums.slice(5);
             }
             if (nums.length <= 8) return nums;
             return nums.slice(0,8) + '-' + nums.slice(8,9);
