53ef794c5ad49e22963bb7c1dcd59f2580ad7799
Baxter <gustavomgonzalez16@gmail.com>
2026-08-22 15:41:50 -0400

    Ajustar inventario para escritorio y laptop

--- controllers/ProductosController.php
+++ controllers/ProductosController.php
@@ -75,7 +75,7 @@ class ProductosController extends Controller
         $this->view('productos/index', [
             'titulo'       => 'Inventario | JV3000 C.A.',
             'wrapper_class' => 'pagina-productos',
-            'css_extra'    => ['modules/productos/productos.css?v=7'],
+            'css_extra'    => ['modules/productos/productos.css?v=8'],
             'js_extra'     => ['modules/productos/productos.js?v=5'],
             'csrf'         => Security::generateToken(),
             'flash'        => $flash,
